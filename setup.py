25f56c25f8467865a6b1c201b8712a1420d6655e
Ivan Pankratov <im.pankratov@ya.ru>
2020-04-04 23:23:03 +0500

    update info

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(name='sony-headphones-control',
       version='0.1',
-      description='',
+      description='Control sony headphones noise cancelling modes',
       url='',
-      author='',
-      author_email='',
-      license='',
+      author='Ivan Pankratov',
+      author_email='im.pankratov@ya.ru',
+      license='MIT',
       packages=['sony-headphones-control'],
       install_requires=[
           'PyBluez',
